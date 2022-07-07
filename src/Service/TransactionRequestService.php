3b08fefa3d3cc88703f4415c934918f1c7238014
jgondellier <jgondellier@yahoo.fr>
2022-07-07 16:44:52 +0200

    add cancel et relaunch transaction

--- src/Service/TransactionRequestService.php
+++ src/Service/TransactionRequestService.php
@@ -16,7 +16,7 @@ class TransactionRequestService
      * TransactionRequestService constructor.
      * @param string $uri url d'appel du service
      */
-    public function __construct($uri)
+    public function __construct(string $uri)
     {
         $this->uri = $uri;
         $this->client = new Client([
@@ -26,14 +26,61 @@ class TransactionRequestService
         ]);
     }
 
+    /**
+     * Requests a new transaction for the client signature service. Sends the document to be signed and other parameters
+     * and returns an URL where the end user should be redirected to. A transaction must be completed whithin 14 days
+     * after its request.
+     *
+     * @param TransactionRequest $transactionRequest La transaction complete en xmlrpc
+     * @return void
+     */
     public function validate(TransactionRequest $transactionRequest)
     {
-        //file_put_contents('RPC_DEBUG_TransactionRequest.xml', xmlrpc_encode_request('requester.requestTransaction',$transactionRequest->getArray()));
         $response = $this->client->request('POST', $this->uri.'/sign/rpc/', [
             'body' => xmlrpc_encode_request('requester.requestTransaction',$transactionRequest->getArray())
         ]);
         $this->originalResult = xmlrpc_decode($response->getBody()->getContents());
         $this->fault = $transactionRequest->checkResponseFault($this->originalResult);
+    }
+
+    /**
+     * Refreshes the creation date for the transaction. The invitation email is sent again if the parameters allow it
+     * (chainingMode equals email and in the case of the first signer, mustContactFirstSigner equals true).
+     * This method can be used to postpone the expiration date of the transaction.
+     *
+     * @param String $transactionId l'id de de transction
+     * @return void
+     */
+    public function cancelTransaction(String $transactionId)
+    {
+        $response = $this->client->request('POST', $this->uri.'/sign/rpc/', [
+            'body' => xmlrpc_encode_request('requester.cancelTransaction',$transactionId)
+        ]);
+
+        $this->originalResult = xmlrpc_decode($response->getBody()->getContents());
+
+        if (is_array($this->originalResult) && xmlrpc_is_fault($this->originalResult)) {
+            $this->fault = $this->originalResult;
+        }
+    }
+
+    /**
+     * Cancel a transaction in progress with this id.
+     *
+     * @param String $transactionId
+     * @return void
+     */
+    public function relaunchTransaction(String $transactionId)
+    {
+        $response = $this->client->request('POST', $this->uri.'/sign/rpc/', [
+            'body' => xmlrpc_encode_request('requester.relaunchTransaction',$transactionId)
+        ]);
+
+        $this->originalResult = xmlrpc_decode($response->getBody()->getContents());
+
+        if (is_array($this->originalResult) && xmlrpc_is_fault($this->originalResult)) {
+            $this->fault = $this->originalResult;
+        }
     }
 
     /**
